6b7dafcfdec362895cf701d741d5a6e62bad460b
Austin Smith <austinpc@austins-mbp.lan>
2020-06-10 19:07:18 -0500

    Added the jQuery code to pulll the JSON file

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpdgEzPVQX5Ub1J_F2cyOSF8/od6/public/values?alt=json' })
+.then(data => console.log(data) )
+.catch((err) => console.log('err -', err));
