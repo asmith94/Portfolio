a045bcf819a6bb21fbc63c87723d55f39d4e66ca
Austin Smith <austinpc@austins-mbp.lan>
2020-06-11 20:29:40 -0500

    Continuing working

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@ $(document).ready(function() {
     myElement.css('font-size', "30px")
     myElement.css('color', 'darkred')
 });
+
+// This code was used from the SEIR js-google-sheet-as-json-using-ajax
+
+$.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpdgEzPVQX5Ub1J_F2cyOSF8/od6/public/values?alt=json' })
+   .then((data) => {
+
+    console.log("data", data);
+    
+    let projects = data.feed.entry.map((project) => {
+    return{  title: project.gsx$title.$t,
+            image: project.gsx$image.$t,
+            description: project.gsx$description.$t,
+            url: project.gsx$url.$t
+    };
+    });
+    app(projects);
+})
+
+function app(projectsArr) {
+    console.log("app - projects", projectsArr);
+    projectsArr.forEach((project) => {
+        let img = $("<img>").attr('scr', project.image);
+        $('.row').append(img);
+    })  
+        // document.getElementById('a-tag').setAttribute('href', projects.url);
+
+    
+   }
+
+   
+
+  
