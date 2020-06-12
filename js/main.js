47e3a0225d96f8cc143a4df2a72f30a97bca4935
Austin Smith <austinpc@Austins-MacBook-Pro.local>
2020-06-12 07:15:11 -0500

    Almost Finished!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
+//I was getting errors accessing the google document, so I found a work-around. Uploading it to this site let me access the data.
 $.ajax({ url:'https://www.xodo.com/app/#/view/49b5b202-9fd4-4ecb-8af5-81df18cd5990' })
 .then(data => console.log(data) )
 .catch((err) => console.log('err -', err));
 
 
-
+//This is a basic css setter function that changes the element through JS
 $(document).ready(function() {
     let myElement = $('#subtext');
     myElement.css('font-size', "30px")
-    myElement.css('color', 'darkred')
+    myElement.css('color', 'blue')
+    myElement.css('max-width', '100%')
+    myElement.css('font-style', 'italic')
 });
 
 // This code was used from the SEIR js-google-sheet-as-json-using-ajax
@@ -17,7 +20,7 @@ $.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpd
 
     console.log("data", data);
     
-    let projects = data.feed.entry.map((project) => {
+    const projects = data.feed.entry.map((project) => {
     return{ title: project.gsx$title.$t,
             image: project.gsx$image.$t,
             description: project.gsx$description.$t,
@@ -25,27 +28,30 @@ $.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpd
     };
     });
     app(projects);
-})
-
-function app(projects) {
-    console.log("app - projects", projects);
-    projects.forEach((project) => {
+   })
+   
+// function app(projectsArr) {
+    
+//     projectsArr.forEach(function(projectsArr) {
     
        
-    
-        $("img#image").attr('src', projects.image);
-        // $('.picture').append(img);
-        $("a#a-tag").prop("href", projects.url);
         
-    })
+//         $("#image").attr('src', projectsArr.image);
+//         // $('.card-img').append(img);
+//         $("#a-tag").prop("href", projectsArr.url);
+        
+//     })
+
+    
+//  }; 
+
+// This is to show that I was able to populate the Object correctly
+ console.log("app - projects", projectsArr);
 
 
-}; 
 
-
-
-
-
+// Unfortunately I was not able to get the function 'app' to work correctly. I spend the entire night working on it,
+// and hopefully by the morning I can get it done.
     
    
 
