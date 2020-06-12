7c76b7da55906d559e978478139a870e9bee1e50
Austin Smith <austinpc@Austins-MacBook-Pro.local>
2020-06-12 07:47:00 -0500

    Time to finish up!

--- js/main.js
+++ js/main.js
@@ -13,6 +13,17 @@ $(document).ready(function() {
     myElement.css('font-style', 'italic')
 });
 
+
+$(document).ready(function() {
+    let myElement = $('#subtext');
+    myElement.css('font-size', "30px")
+    myElement.css('color', 'blue')
+    myElement.css('max-width', '100%')
+    myElement.css('font-style', 'italic')
+});
+
+
+
 // This code was used from the SEIR js-google-sheet-as-json-using-ajax
 
 $.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpdgEzPVQX5Ub1J_F2cyOSF8/od6/public/values?alt=json' })
