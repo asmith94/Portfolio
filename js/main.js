0801a8eb139b4229d84abd1cd8369a07ffa08f2f
Austin Smith <austinpc@Austins-MacBook-Pro.local>
2020-06-12 02:51:24 -0500

    More work being done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpdgEzPVQX5Ub1J_F2cyOSF8/od6/public/values?alt=json' })
+$.ajax({ url:'https://docs.google.com/spreadsheets/d/1HY3tz_1B-5GHfX_9yDpdpdgEzPVQX5Ub1J_F2cyOSF8/edit?usp=sharing' })
 .then(data => console.log(data) )
 .catch((err) => console.log('err -', err));
+
+var htmlOutput = template.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME);
 
 $(document).ready(function() {
     let myElement = $('#subtext');
@@ -16,7 +18,7 @@ $.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpd
     console.log("data", data);
     
     let projects = data.feed.entry.map((project) => {
-    return{  title: project.gsx$title.$t,
+    return{ title: project.gsx$title.$t,
             image: project.gsx$image.$t,
             description: project.gsx$description.$t,
             url: project.gsx$url.$t
@@ -28,13 +30,21 @@ $.ajax({ url:'https://spreadsheets.google.com/feeds/list/1HY3tz_1B-5GHfX_9yDpdpd
 function app(projectsArr) {
     console.log("app - projects", projectsArr);
     projectsArr.forEach((project) => {
-        let img = $("<img>").attr('scr', project.image);
-        $('.row').append(img);
-    })  
-        // document.getElementById('a-tag').setAttribute('href', projects.url);
+    
+        
+        $("#image").attr('src', project.image);
+        // $('.picture').append(img);
+        $("a#a-tag").prop("href", project.url)
+    })
+
+
+}; 
+
+
+
 
     
-   }
+   
 
    
 
